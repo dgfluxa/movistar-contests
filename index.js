b55f97d5ed722b957bf7b4cc18d3fab2c1d25633
dgfluxa <dgfluxa@uc.cl>
2023-01-04 18:44:34 -0300

    Fixed error by scrolling into bottom of page (show element)

--- index.js
+++ index.js
@@ -1,64 +1,75 @@
-const puppeteer = require('puppeteer');
-require('dotenv').config()
+const puppeteer = require("puppeteer");
+require("dotenv").config();
 
 // Helper funtions to click on elements by specified text
-// REcovered and adapted from: https://gist.github.com/tokland/d3bae3b6d3c1576d8700405829bbdb52
-const escapeXpathString = str => {
-    const splitedQuotes = str.replace(/'/g, `', "'", '`);
-    return `concat('${splitedQuotes}', '')`;
-  };
+// Recovered and adapted from: https://gist.github.com/tokland/d3bae3b6d3c1576d8700405829bbdb52
+const escapeXpathString = (str) => {
+  const splitedQuotes = str.replace(/'/g, `', "'", '`);
+  return `concat('${splitedQuotes}', '')`;
+};
+
+const clickByText = async (page, text) => {
+  const escapedText = escapeXpathString(text);
+  const linkHandlers = await page.$x(`//*[contains(text(), ${escapedText})]`);
   
-  const clickByText = async (page, text) => {
-    const escapedText = escapeXpathString(text);
-    const linkHandlers = await page.$x(`//*[contains(text(), ${escapedText})]`);
-    
-    if (linkHandlers.length > 0) {
-      await linkHandlers[0].click();
-    } else {
-      throw new Error(`Link not found: ${text}`);
-    }
-  };
+  if (linkHandlers.length > 0) {
+    await linkHandlers[0].click();
+  } else {
+    throw new Error(`Link not found: ${text}`);
+  }
+};
 
 async function run(contestText) {
-    // Launch the browser
-    const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
-    // Create a new page
-    const page = await browser.newPage();
-    // Go to log in page
-    await page.goto('https://acceso.movistar.cl/SSO_AU_WEB/loginAction.do?_ga=2.129519564.1703626475.1669748900-1960901754.1669748900');
-    // Type in username
-    await page.type('#username', process.env.RUT);
-    // Type in password
-    await page.type('#password', process.env.PASSWORD);
-    // Click on the login button
-    await page.click('#botonLogin');
-    // Wait for the page to load
-    await page.waitForSelector('#card-revisa-consumo');
-    // Go to club-movistar page
-    await page.goto('https://mi.movistar.cl/svr/#/main/club-movistar');
-    // Wait for club-movistar page to load
-    await page.waitForNavigation({waitUntil: 'networkidle0'});  
-    // Wait for iframe
-    const elementHandle = await page.waitForSelector('.content-iframe');
-    // Get the iframe element
-    const frame = await elementHandle.contentFrame();
-    // Wait for contest button to load
-    await frame.waitForSelector('.ion-no-padding.ion-no-margin.ion-text-center.ion-align-self-center.nv-padding-8.cursePointer.opcion-no-active.md.hydrated'); 
-    // Go to contest list by clicking "Concursos" button
-    await frame.click('.ion-no-padding.ion-no-margin.ion-text-center.ion-align-self-center.nv-padding-8.cursePointer.opcion-no-active.md.hydrated');
-    // Wait some time for desired contest to load
-    await new Promise(r => setTimeout(r, 500));
-    // Click on desired contest
-    await clickByText(frame, contestText);
-    // Wait some time for page to load
-    await new Promise(r => setTimeout(r, 500));
-    // Take screenshot
-    await page.screenshot({ path: 'screenshot.png' });
-    // Close the browser
-    await browser.close();
+  // Launch the browser
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+  // Create a new page
+  const page = await browser.newPage();
+  // Go to log in page
+  await page.goto(
+    "https://acceso.movistar.cl/SSO_AU_WEB/loginAction.do?_ga=2.129519564.1703626475.1669748900-1960901754.1669748900"
+  );
+  // Type in username
+  await page.type("#username", process.env.RUT);
+  // Type in password
+  await page.type("#password", process.env.PASSWORD);
+  // Click on the login button
+  await page.click("#botonLogin");
+  // Wait for the page to load
+  await page.waitForSelector("#card-revisa-consumo");
+  // Go to club-movistar page
+  await page.goto("https://mi.movistar.cl/svr/#/main/club-movistar");
+  // Wait for club-movistar page to load
+  await page.waitForNavigation({ waitUntil: "networkidle0" });
+  // Wait for iframe
+  const elementHandle = await page.waitForSelector(".content-iframe");
+  // Get the iframe element
+  const frame = await elementHandle.contentFrame();
+  // Wait for contest button to load
+  await frame.waitForSelector(
+    ".ion-no-padding.ion-no-margin.ion-text-center.ion-align-self-center.nv-padding-8.cursePointer.opcion-no-active.md.hydrated"
+  );
+  // Go to contest list by clicking "Concursos" button
+  await frame.click(
+    ".ion-no-padding.ion-no-margin.ion-text-center.ion-align-self-center.nv-padding-8.cursePointer.opcion-no-active.md.hydrated"
+  );
+  // Wait some time for desired contest to load (Just in case)
+  await new Promise((r) => setTimeout(r, 500));
+  // Scroll to bottom of page
+  await page.keyboard.down('End')
+  // Wait some time to reach bottom of page (Just in case)
+  await new Promise((r) => setTimeout(r, 500));
+  // Click on desired contest
+  await clickByText(frame, contestText);
+  // Wait some time for page to load (Just in case)
+  await new Promise((r) => setTimeout(r, 500));
+  // Take screenshot
+  await page.screenshot({ path: "screenshot.png" });
+  // Close the browser
+  await browser.close();
 }
 
-contestText = 'concurso movistar arena 13 de enero';
+contestText = "concurso movistar arena 13 de enero";
 run(contestText);
-
-
